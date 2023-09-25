e045c7decce3ccd1d31fd6ea6c360f23c1e8d6c5
arizoro <arieman69@gmail.com>
2023-09-25 23:31:51 +0700

    btn dowload on

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ const type = new Typed(".input",{
     startDelay: 1500,
     backDelay: 500
 })
+
+function loadFile(url, fileName) {
+    fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
+        .then(res => res.blob())
+        .then(res => {
+            const aElement = document.createElement('a');
+            aElement.setAttribute('download', fileName);
+            const href = URL.createObjectURL(res);
+            aElement.href = href;
+            aElement.setAttribute('target', '_blank');
+            aElement.click();
+            URL.revokeObjectURL(href);
+        });
+};
+
+function downloadFile(){
+    loadFile('/assets/mycv.pdf', 'mycv.pdf')
+}
